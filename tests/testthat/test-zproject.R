2c135cf6ac82030189657ed27ae801409720aff7
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-08-01 19:34:59 +0300

    Add test for the presence of Z tutorial folder

--- tests/testthat/test-zproject.R
+++ tests/testthat/test-zproject.R
@@ -1,3 +1,12 @@
-context("Zproject creation length")
+context("Zproject creation")
 
-# Using tutorial data
+# FIXME: there must a smarter way than then the relative path below...
+tutorial.dir <- file.path("../../", get.tutorialdir())
+
+# Using tutorial data, must be present!
+test_that("Zonation tutorial data is available", {
+  
+  expect_true(file.exists(tutorial.dir), 
+              "Tutorial directory not found in the root of the package.")
+  
+})
