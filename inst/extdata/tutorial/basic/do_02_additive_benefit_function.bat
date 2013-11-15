bb8b0ecaf5ea7700be61d61e80e65adbd86c387e
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-15 13:20:55 +0200

    Add Zonation tutorial basic data and setups

---
+++ inst/extdata/tutorial/basic/do_02_additive_benefit_function.bat
@@ -0,0 +1,1 @@
+call zig3.exe -r 02_additive_benefit_function/02_additive_benefit_function.dat 02_additive_benefit_function/02_additive_benefit_function.spp basic_output/02_additive_benefit_function/output_02_additive_benefit_function.txt 0.0 0 1.0 0
