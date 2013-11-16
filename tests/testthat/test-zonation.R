6985de6ba39417a27cee28583a25b7ac244fd45e
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-16 21:19:45 +0200

    Disable zig tests
    Not reasonable to depend on Zonation being installed on the platform. Since tutorial results are now included in the package, it is not necessary to run Zonation.

--- tests/testthat/test-zonation.R
+++ tests/testthat/test-zonation.R
@@ -6,12 +6,6 @@ bat.file <- system.file("extdata/tutorial/basic", "do_01_core_area_zonation.bat"
 faulty.bat.file <- file.path(dirname(bat.file), "wrong.bat")
 
 test_that("Zonation executable checking works", {
-  # Zig3
-  expect_true(check_zonation(), 
-              "Zonation v3 (zig3) not found in the system.")
-  # Zig4
-  expect_true(check_zonation("zig4"), 
-              "Zonation v4 (zig4) not found in the system.")
   # Version not found
   expect_warning(check_zonation("zig5"),
               "Zonation executable zig5 not found in the system.")
