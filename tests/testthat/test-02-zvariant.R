e20a6cf94ae778394f0e5afdca694d628d5de1c9
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-27 17:56:29 +0200

    Add functionality for assigning and getting group names

--- tests/testthat/test-02-zvariant.R
+++ tests/testthat/test-02-zvariant.R
@@ -39,10 +39,20 @@ test_that("Zvariant with results is created correctly", {
   expect_true(.hasSlot(results.variant, "groups"),
               "Test variant object doesn't have a slot 'groups'")
   
-  correct.grp.levels <- factor(c(1, 2, 2, 1, 2, 1, 1))
-  variant.grp.levels <- groups(results.variant)
-  expect_identical(variant.grp.levels, correct.grp.levels, 
+  correct.grp.codes <- c(1, 2, 2, 1, 2, 1, 1)
+  variant.grp.codes <- groups(results.variant)
+  expect_identical(variant.grp.codes, correct.grp.codes, 
               paste("Test variant group information wrong"))
+  
+  # The group names haven't been set, so there should be none
+  expect_true(is.na(groupnames(results.variant)),
+                   "Test variant group names not NA although they haven't been set")
+  
+  correct.grp.names <- c("mammals", "owls")
+  names(correct.grp.names) <- c(1, 2)
+  groupnames(results.variant) <- correct.grp.names
+  expect_identical(correct.grp.names, groupnames(results.variant),
+                   "Test variant group names not what they're supposed to")
   
 })
 
@@ -53,9 +63,14 @@ test_that("Zvariant without results is created correctly", {
                                    "06_dummy_for_testing.batx")
   no.results.variant <- new("Zvariant", bat.file=no.results.bat.file)
   
+  # Groups
   expect_false(has_results(no.results.variant),
                "Test variant should not have results")
   expect_true(is.na(groups(no.results.variant)),
                "Test variant should not have groups")
+  
+  correct.grp.names <- c("mammals", "owls")
+  names(correct.grp.names) <- c(1, 2)
+  expect_error((groupnames(no.results.variant) <- correct.grp.names))
 }
 )
