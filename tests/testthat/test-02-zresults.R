8575e0153baf645a4f5001d12860ea7f3885e161
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-29 14:36:23 +0200

    Method 'performance' added

--- tests/testthat/test-02-zresults.R
+++ tests/testthat/test-02-zresults.R
@@ -52,4 +52,44 @@ test_that("getting curves works", {
   expect_identical(correct.grp.curves, curves(test.results, groups=TRUE),
                    "Test results object's group curves data incorrect") 
 
-  })
+})
+
+test_that("performance levels are reported right", {
+  
+  results.path <- file.path(.options$output.dir, "01_core_area_zonation")
+  test.results <- new("Zresults", root=results.path)
+  
+  curves.file <- file.path(results.path, "01_core_area_zonation.curves.txt")
+  
+  correct.curves <- read_curves(curves.file)
+  
+  # Test for individual feature for several thresholds
+  # Get the right rows
+  breaks <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
+  row.ids <- sapply(breaks, function(x) {which(correct.curves$pr_lost >= x)[1]})
+  
+  levels.all <- correct.curves[row.ids, c(1, 8:ncol(correct.curves))]
+  
+  # Don't allow values < 0 or > 1 for pr.lost
+  expect_error(performance(test.results, pr.lost=-0.1))
+  expect_error(performance(test.results, pr.lost=1.1))
+  
+  # Get all features
+  expect_identical(levels.all, performance(test.results, pr.lost=breaks),
+                   "Method performance doesn't return levels right")
+  
+  # Get levels for a specific feature/features
+  levels.feat1 <- data.frame(pr_lost=levels.all$pr_lost, f1=levels.all$f1)
+  expect_identical(levels.feat1, performance(test.results, pr.lost=breaks,
+                                             features="f1"),
+                   "Method performance doesn't return level for 1 feature right")
+  levels.feats1.5 <- data.frame(pr_lost=levels.all$pr_lost, f1=levels.all$f1,
+                                f5=levels.all$f5)
+  expect_identical(levels.feats1.5, performance(test.results, pr.lost=breaks,
+                                                features=c("f1", "f5")),
+                   "Method performance doesn't return levels for several features right")
+  # Test for invalid feature name
+  expect_warning(performance(test.results, pr.lost=breaks, features="fX1"))
+  suppressWarnings(expect_true(is.na(performance(test.results, pr.lost=breaks, 
+                                                 features="fX1"))))
+})
