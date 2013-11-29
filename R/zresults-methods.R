5101c1a94a8ec13c7babb501301a30af4ffec75d
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-29 14:36:23 +0200

    Method 'performance' added

--- R/zresults-methods.R
+++ R/zresults-methods.R
@@ -42,3 +42,65 @@ setMethod("curves", c("Zresults"), function(x, groups=FALSE) {
     return(x@curves)
   }
 })
+
+#' Get performance levels either for features or groups from a \code{Zresults} 
+#' object.
+#' 
+#' @param x \code{Zresults} object.
+#' @param pr.lost numeric vector containing the fractions of landscape lost
+#' for which the feature/group performance values are wanted (default: 'all').
+#' @param features character vector of features names to be extracted. Must 
+#' match with feature names in curves data
+#' @param groups logical indicating whether group curves data should be 
+#' used (default: FALSE).
+#'
+#' @return Data frame containing the curves file data for selected fractions
+#' of landscape lost. First column is pr_lost. If feature names are provided and 
+#' none are viable, return NA.
+#' 
+#' @seealso \code{\link{Zresults-class}} \code{\link{read_curves}} 
+#' \code{\link{read_grp_curves}}
+#' 
+#' @export
+#' @docType methods
+#' @rdname zvariant-methods
+#' 
+#' @author Joona Lehtomaki \email{joona.lehtomaki@@gmail.com}
+#' 
+setGeneric("performance", function(x, pr.lost, features="all", groups=FALSE) {
+  standardGeneric("performance")
+})
+
+#' @rdname zvariant-methods
+#' @aliases performance,Zvariant-method
+#' 
+setMethod("performance", c("Zresults"), function(x, pr.lost, features="all",
+                                                 groups=FALSE) {
+
+  if (any(pr.lost < 0 || pr.lost > 1.0)) {
+    stop("Proportion landscape lost (pr.lost) values must be 0 < pr.lost < 1")
+  }
+  curves <- curves(x)
+  row.ids <- sapply(breaks, function(y) {which(curves$pr_lost >= y)[1]})
+  feature.curves <- curves[row.ids, c(1, 8:ncol(curves))]
+
+  if (features[1] != "all") {
+    #browser()
+    feature.names <- names(feature.curves)
+    if (!all(features %in% feature.names)){
+      warning(paste("Feature names", 
+                    paste(features[!all(features %in% feature.names)], 
+                          collapse=", "),
+                    "not found in curves header"))
+      features <- features[all(features %in% feature.names)]
+      if (length(features) == 0) {
+        return(NA)
+      }
+    }
+    inds <- sapply(features, function(y) {which(y == feature.names)})
+    feature.curves <- feature.curves[c(1, inds)]
+    row.names(feature.curves) <- 1:nrow(feature.curves)
+  }
+  return(feature.curves)
+  
+})
