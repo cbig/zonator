dceba79dec4a4d537c1feb78e074f6d881383fb1
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-08-13 18:01:29 +0300

    Fix Z tutorial paths so that there's no warning
    For excessive length

--- inst/extdata/tutorial/basic/01_core_area_zonation.bat
+++ inst/extdata/tutorial/basic/01_core_area_zonation.bat
@@ -1,1 +1,1 @@
-call zig3.exe -r 01_core_area_zonation/01_core_area_zonation.dat 01_core_area_zonation/01_core_area_zonation.spp basic_output/01_core_area_zonation/01_core_area_zonation.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
+call zig3.exe -r 01/01.dat 01/01.spp output/01/01.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
