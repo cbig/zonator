0bf5b73e978e13279db8a5ac53452c2435ae1de7
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-06-09 22:46:14 +0300

    Create spp files based on folder of rasters
    create_zproject() also works with this.

--- tests/testthat/test-06-zonation.R
+++ tests/testthat/test-06-zonation.R
@@ -5,3 +5,28 @@ test_that("Zonation executable checking works", {
   expect_warning(check_zonation("zig5"),
                  info="Exe-not-found error not reported right")
 })
+
+context("Creating Zonation input files")
+
+test_that("Creating a spp file based on a directory of input rasters works", {
+  
+  # This is what the spp file is supposed to look like
+  correct_spp_file <- .options$spp.file
+  correct_spp_content <- read_spp(correct_spp_file)
+  row.names(correct_spp_content) <- NULL
+  
+  # Generate a temporary spp file
+  temp_spp_file <- tempfile(fileext = ".spp")
+  
+  create_spp(filename = temp_spp_file, weight = correct_spp_content$weight, 
+             alpha = correct_spp_content$alpha, bqp = correct_spp_content$bqp, 
+             bqp_p = correct_spp_content$bqp_p, 
+             cellrem = correct_spp_content$cellrem, 
+             spp_file_dir = .options$data.dir, 
+             spp_file_pattern = "(species)[1-9]{1}\\.(tif)", 
+             override_path = "../data")
+  
+  # Compare the content of files
+  expect_equal(correct_spp_content, read_spp(temp_spp_file),
+               info = "Generated spp file content not the same as template") 
+})
