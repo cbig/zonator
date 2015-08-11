8c1fe56f6b7c68a0b53d4be488197320dc3f3356
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-08-11 13:34:37 +0300

    Update tutorial bats
    * New output folder location within variant folders
    * Also update to zig4.exe

--- inst/extdata/tutorial/basic/04_distribution_smoothing.bat
+++ inst/extdata/tutorial/basic/04_distribution_smoothing.bat
@@ -1,1 +1,1 @@
-call zig3.exe -r 04/04.dat 04/04.spp output/04/04.txt 0.0 1 2 0 --grid-output-formats=compressed-tif --image-output-formats=png
+call zig4.exe -r 04/04.dat 04/04.spp 04_out/04.txt 0.0 1 2 0 --grid-output-formats=compressed-tif --image-output-formats=png
--- inst/extdata/tutorial/basic/05_hierarchical_removal_mask.bat
+++ inst/extdata/tutorial/basic/05_hierarchical_removal_mask.bat
@@ -1,1 +1,1 @@
-call zig3.exe -r 05/05.dat 05/05.spp output/05/05.txt 0.5 1 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
+call zig4.exe -r 05/05.dat 05/05.spp 05/05_out/05.txt 0.5 1 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png
