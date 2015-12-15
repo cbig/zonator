b27733808024a709200bdac1b9bf56bf0a595b4f
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-12-15 15:15:45 +0200

    Remove obsolete functions
    1. create_batch_file()
    2. create_spp_file()

--- R/write-input.R
+++ R/write-input.R
@@ -11,24 +11,5 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of 
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-create_batch_file <- function(filename = "run_multiple.bat", exe="zig2c.exe",
-							  param="-r", dat, spp, output, uc=0.0, ds=0,
-							  am=1.0, win=1, append=FALSE) {
-	line <- paste("call", exe, param, dat, spp, output, uc, ds, am, win, "\n")
 
-	task <- ifelse(append, "edited", "created")
-	cat(line, file=filename, append=append)
-
-	cat(paste("\n<<<", task, "batch file"), filename)
-}
-
-create_spp_file <- function(filename="filelist.spp", weight=1.0, alpha=1.0,
-							bqp=1, bqp.p=1, cellrem=1, sppfiles) {
-	for (i in 1:length(sppfiles)) {
-		append <- ifelse (i == 1, FALSE, TRUE)
-		line <- paste(weight, alpha, bqp, bqp.p, cellrem, sppfiles[i], "\n")
-		cat(line, file=filename, append=append)
-	}
-	cat(paste("\n<<< Created spp file"), filename)
-}
 
