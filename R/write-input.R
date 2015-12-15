ef0084b2a23f4beba6b160683270f85acde0b1bc
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-12-15 15:29:47 +0200

    Start implementation of write_dat().

--- R/write-input.R
+++ R/write-input.R
@@ -11,5 +11,20 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of 
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+#' Write a Zonation run configuration (dat) file.
+#' 
+#' Checks a vector of names only contains unique items and if they're not,
+#' unique names will be created. Also, the items must be
+#' suitable for columns names. Function is strict so that if the vector is not 
+#' valid or it cannot be coerced to be one an error is induced. 
+#' 
+#' @param x Charcter or numeric vector.
+#'
+#' @return Valid vector of the original size.
+#' 
+#' @author Joona Lehtomaki \email{joona.lehtomaki@@gmail.com}
+#' @export
+#'  
+write_dat <- function(x) {
 
-
+  }
---
+++ tests/testthat/test-03-writing.R
@@ -0,0 +1,17 @@
+context("Writing Zonation input files")
+
+test_that("Writing a dat file works", {
+  
+  dat_file <- system.file("extdata/tutorial/basic/01", "01.dat",
+                          package="zonator")
+  
+  org_dat_data <- read_dat(dat_file)
+  
+  # Change removal rule to 2 (ABF)
+  org_dat_data$Settings$removal_rule <- 2
+  # Write file, expect message informing about the write operation
+  dat_file_path <- tempfile(fileext = ".dat")
+  expect_message(write_dat(x = org_dat_data, file = dat_file_path),
+                 paste("Wrote dat-file", dat_file_path),
+                 info = "Correct message not emitted from writing a dat file.")
+})
