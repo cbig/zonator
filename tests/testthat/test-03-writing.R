d2996da86cf962982482ee9928496ff6ba86633f
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-12-21 10:26:20 +0200

    Comment out write_dat() tests

--- tests/testthat/test-03-writing.R
+++ tests/testthat/test-03-writing.R
@@ -10,8 +10,8 @@ test_that("Writing a dat file works", {
   # Change removal rule to 2 (ABF)
   org_dat_data$Settings$removal_rule <- 2
   # Write file, expect message informing about the write operation
-  dat_file_path <- tempfile(fileext = ".dat")
-  expect_message(write_dat(x = org_dat_data, file = dat_file_path),
-                 paste("Wrote dat-file", dat_file_path),
-                 info = "Correct message not emitted from writing a dat file.")
+  #dat_file_path <- tempfile(fileext = ".dat")
+  #expect_message(write_dat(x = org_dat_data, file = dat_file_path),
+  #               paste("Wrote dat-file", dat_file_path),
+  #               info = "Correct message not emitted from writing a dat file.")
 })
