ee61065163661b517b27eecf6eec4576f742f79a
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-02-02 22:10:02 +0100

    Add check scripts

---
+++ inst/extras/build.cran.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CURRENT_TAR=zonator_0.5.6.tar.gz
+
+#/usr/bin/R CMD BATCH document.R
+#/usr/bin/R CMD build ../../ --no-build-vignettes
+/usr/bin/R CMD build ../../
+/usr/bin/R CMD check --as-cran $CURRENT_TAR
+#/usr/bin/R CMD INSTALL $CURRENT_TAR
+#/usr/bin/R CMD BATCH document.R
